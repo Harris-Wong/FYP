58466b70712d326809aa0e6de9bb1c542949a8e3
BorisTam0902 <61449139+BorisTam0902@users.noreply.github.com>
2023-04-13 14:09:56 +0800

    Update cron_task.js

--- models/cron_task.js
+++ models/cron_task.js
@@ -8,16 +8,20 @@ async function predictionTask() {
     console.log('Data Fetch completed');
 
     // Run the Python files in order
-    await runProcess('python', ['Data_Scrap_Prediction_1.py']);
+    //await runProcess('python', ['Data_Scrap_Prediction_1.py']);
+    await runProcess('python3', ['Data_Scrap_Prediction_1.py']);
     console.log('Data Scrap Prediction 1/4 completed');
 
-    await runProcess('python', ['Data_Scrap_Prediction_2.py']);
+    //await runProcess('python', ['Data_Scrap_Prediction_2.py']);
+    await runProcess('python3', ['Data_Scrap_Prediction_2.py']);
     console.log('Data Scrap Prediction 2/4 completed');
 
-    await runProcess('python', ['Data_Scrap_Prediction_3.py']);
+    //await runProcess('python', ['Data_Scrap_Prediction_3.py']);
+    await runProcess('python3', ['Data_Scrap_Prediction_3.py']);
     console.log('Data Scrap Prediction 3/4 completed');
-    
-    await runProcess('python', ['Data_Scrap_Prediction_4.py']);
+
+    //await runProcess('python', ['Data_Scrap_Prediction_4.py']);
+    await runProcess('python3', ['Data_Scrap_Prediction_4.py']);
     console.log('Data Scrap Prediction 4/4 completed');
     
     console.log('All Predictions completed!');
